9da2a37d138223f31bcfd772be9cac5448a7da90
sleeman-nabwani <sleeman4268@gmail.com>
2025-03-13 19:39:17 +0200

    initial commit

---
+++ code/main.py
@@ -0,0 +1,56 @@
+import torch
+from torchvision import datasets, transforms
+import numpy as np
+from matplotlib import pyplot as plt
+from utils import plot_tsne
+import numpy as np
+import random
+import argparse
+
+NUM_CLASSES = 10
+
+def freeze_seeds(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+def get_args():   
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', default=0, type=int, help='Seed for random number generators')
+    parser.add_argument('--data-path', default="/datasets/cv_datasets/data", type=str, help='Path to dataset')
+    parser.add_argument('--batch-size', default=8, type=int, help='Size of each batch')
+    parser.add_argument('--latent-dim', default=128, type=int, help='encoding dimension')
+    parser.add_argument('--device', default='cuda' if torch.cuda.is_available() else 'cpu', type=str, help='Default device to use')
+    parser.add_argument('--mnist', action='store_true', default=False,
+                        help='Whether to use MNIST (True) or CIFAR10 (False) data')
+    parser.add_argument('--self-supervised', action='store_true', default=False,
+                        help='Whether train self-supervised with reconstruction objective, or jointly with classifier for classification objective.')
+    return parser.parse_args()
+    
+
+if __name__ == "__main__":
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  #one possible convenient normalization. You don't have to use it.
+    ])
+
+    args = get_args()
+    freeze_seeds(args.seed)
+                
+                                           
+    if args.mnist:
+        train_dataset = datasets.MNIST(root=args.data_path, train=True, download=False, transform=transform)
+        test_dataset = datasets.MNIST(root=args.data_path, train=False, download=False, transform=transform)
+    else:
+        train_dataset = datasets.CIFAR10(root=args.data_path, train=True, download=True, transform=transform)
+        test_dataset = datasets.CIFAR10(root=args.data_path, train=False, download=True, transform=transform)
+        
+    # When you create your dataloader you should split train_dataset or test_dataset to leave some aside for validation
+
+    #this is just for the example. Simple flattening of the image is probably not the best idea                                        
+    encoder_model = torch.nn.Linear(32*32*3,args.latent_dim).to(args.device)
+    decoder_model = torch.nn.Linear(args.latent_dim,32*32*3 if args.self_supervised else NUM_CLASSES).to(args.device) 
+
+    sample = train_dataset[0][0][None].to(args.device) #This is just for the example - you should use a dataloader
+    output = decoder_model(encoder_model(sample.flatten()))
+    print(output.shape)
+
---
+++ code/utils.py
@@ -0,0 +1,54 @@
+import torch
+import numpy as np
+from sklearn.manifold import TSNE
+
+def plot_tsne(model, dataloader, device):
+    '''
+    model - torch.nn.Module subclass. This is your encoder model
+    dataloader - test dataloader to over over data for which you wish to compute projections
+    device - cuda or cpu (as a string)
+    '''
+    model.eval()
+    
+    images_list = []
+    labels_list = []
+    latent_list = []
+    
+    with torch.no_grad():
+        for data in dataloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            
+            #approximate the latent space from data
+            latent_vector = model(images)
+            
+            images_list.append(images.cpu().numpy())
+            labels_list.append(labels.cpu().numpy())
+            latent_list.append(latent_vector.cpu().numpy())
+    
+    images = np.concatenate(images_list, axis=0)
+    labels = np.concatenate(labels_list, axis=0)
+    latent_vectors = np.concatenate(latent_list, axis=0)
+    
+    # Plot TSNE for latent space
+    tsne_latent = TSNE(n_components=2, random_state=0)
+    latent_tsne = tsne_latent.fit_transform(latent_vectors)
+    
+    plt.figure(figsize=(8, 6))
+    scatter = plt.scatter(latent_tsne[:, 0], latent_tsne[:, 1], c=labels, cmap='tab10', s=10)  # Smaller points
+    plt.colorbar(scatter)
+    plt.title('t-SNE of Latent Space')
+    plt.savefig('latent_tsne.png')
+    plt.close()
+    
+    #plot image domain tsne
+    tsne_image = TSNE(n_components=2, random_state=42)
+    images_flattened = images.reshape(images.shape[0], -1)
+    image_tsne = tsne_image.fit_transform(images_flattened)
+    
+    plt.figure(figsize=(8, 6))
+    scatter = plt.scatter(image_tsne[:, 0], image_tsne[:, 1], c=labels, cmap='tab10', s=10)  
+    plt.colorbar(scatter)
+    plt.title('t-SNE of Image Space')
+    plt.savefig('image_tsne.png')
+    plt.close()
